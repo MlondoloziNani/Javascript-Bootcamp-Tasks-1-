beab7d250447b24897f136facccd5d3975bdc08d
MlondoloziNani <nani.mlondolozi@gmail.com>
2025-04-10 15:21:04 +0200

    Datatypes & Operators - Exercise 1

---
+++ datatypes_operators.js
@@ -0,0 +1,20 @@
+//Exercise 1: Numbers
+let integerNumber = 10;
+
+let floatingNumber = 5.5;
+
+let addition = integerNumber + floatingNumber;
+let subtraction = integerNumber - floatingNumber;
+let multiplication = integerNumber * floatingNumber;
+let division = integerNumber / floatingNumber;
+let modulus = integerNumber % floatingNumber;
+let exponentiation = integerNumber ** 2;
+
+console.log("Integer Number:", integerNumber);
+console.log("Floating Number:", floatingNumber);
+console.log("Addition:", addition);
+console.log("Subtraction:", subtraction);
+console.log("Multiplication:", multiplication);
+console.log("Division:", division);
+console.log("Modulus:", modulus);
+console.log("Exponentiation:", exponentiation);
