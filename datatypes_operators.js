5f9a80bcf0600a39005d0b53722a5c7f1b4f0056
MlondoloziNani <nani.mlondolozi@gmail.com>
2025-04-10 16:03:33 +0200

    Exercise 2

--- datatypes_operators.js
+++ datatypes_operators.js
@@ -18,3 +18,43 @@ console.log("Multiplication:", multiplication);
 console.log("Division:", division);
 console.log("Modulus:", modulus);
 console.log("Exponentiation:", exponentiation);
+
+//Exercise 2: Boolean and Operators
+
+//Q1
+let compare = 14 > 3;
+
+//Q2
+let x = 8;
+let y = 12
+
+console.log(x > y);
+console.log(x <= y);
+console.log(x == y);
+console.log(x != y);
+
+//Q3
+let a = true;
+let b = false;
+
+console.log(a && b);
+console.log(a || b);
+console.log(!a);
+
+//Q4
+let p = 10;
+
+p += 7;
+console.log(p);
+
+p -= 2;
+console.log(p);
+
+p *= 4;
+console.log(p);
+
+p /= 2;
+console.log(p);
+
+p %= 3.5;
+console.log(p);
