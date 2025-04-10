ae3a5e333f7bcee6a5ebf6b8c4a60cb65091ec3e
MlondoloziNani <nani.mlondolozi@gmail.com>
2025-04-10 14:24:46 +0200

    Exercise 3

--- variables.js
+++ variables.js
@@ -2,7 +2,10 @@
 let name = "Mlondolozi";
 let age = 31;
 let isStudent = true;
-console.log('Name:', name, '\nAge:', age, '\nisStudent:', isStudent);
+
+console.log('Name:', name,
+            '\nAge:', age,
+            '\nisStudent:', isStudent);
 
 //Exercise 2 Variable
 let favoriteColour = "Grey";
@@ -11,4 +14,16 @@ console.log('Favorite colour', favoriteColour);
 favoriteColour = "Black";
 console.log('Favorite colour', favoriteColour);
 
-//
+//Exercise 3: Arithmetic
+let num1 = 34;
+let num2 = 15;
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product =  num1 * num2;
+let quotient = num1 / num2;
+
+console.log('Sum:', sum,
+            '\nDifference:', difference,
+            '\nProduct:', product,
+            '\nQuotient:', quotient)
