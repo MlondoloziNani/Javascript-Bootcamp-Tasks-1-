43601a389e6729a948b733d0d1cb027e2b4f57c6
MlondoloziNani <nani.mlondolozi@gmail.com>
2025-04-10 14:06:57 +0200

    Exercise 2

--- variables.js
+++ variables.js
@@ -1,4 +1,14 @@
+//Exercise 1: Variable Declaration
 let name = "Mlondolozi";
 let age = 31;
 let isStudent = true;
 console.log('Name:', name, '\nAge:', age, '\nisStudent:', isStudent);
+
+//Exercise 2 Variable
+let favoriteColour = "Grey";
+console.log('Favorite colour', favoriteColour);
+
+favoriteColour = "Black";
+console.log('Favorite colour', favoriteColour);
+
+//
